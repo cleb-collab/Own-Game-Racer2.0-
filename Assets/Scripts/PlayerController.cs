1ac0849ac4d8d36d33a2adb192d80c07583b859c
cleb-collab <CLeberr@nycstudents.net>
2024-04-08 12:41:00 -0400

    Day2

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float speed = 5;
+    public float turnSpeed = 50;
+    public float horizontalInput;
+    private float verticalInput;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        // Moves the car forward
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput);
+        // Turns the car on the Y axis
+        transform.Rotate(Vector3.up * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
